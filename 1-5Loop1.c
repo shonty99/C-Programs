936d5571647e6d7bb2547bd2b1cd6f5b139f6eb3
shonty99 <105678948+shonty99@users.noreply.github.com>
2022-05-24 01:53:45 +0530

    Create 1-5Loop1.c

---
+++ 1-5Loop1.c
@@ -0,0 +1,33 @@
+// Program to display 1 to 5 on screen by Using Loop and Updator
+// Output   1   2   3   4   5
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+        int iCnt = 0;
+        if(iNo < 0)
+        {
+            iNo = -iNo;
+        }
+        
+        //       1                2             3
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            printf("%d\n",iCnt);        // 4
+        }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);    // Display(4);
+
+    return 0;
+}
