dcf20ab8fee27176451b9a08adefc145b2341eee
shonty99 <105678948+shonty99@users.noreply.github.com>
2022-05-24 01:31:15 +0530

    Create HelloLoop.c

---
+++ HelloLoop.c
@@ -0,0 +1,25 @@
+// Program to display 5 times Hello on screen With using Function
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+
+#include<stdio.h>
+
+// Demonstration of SEQUANCE
+void Display()
+{
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
