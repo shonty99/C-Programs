1a9d6f66fe97b92b5886f961a0494833baad4c8c
shonty99 <105678948+shonty99@users.noreply.github.com>
2022-05-24 01:29:45 +0530

    Create 5TimesHello.c

---
+++ 5TimesHello.c
@@ -0,0 +1,21 @@
+// Program to display 5 times Hello on screen
+//Hello
+//Hello
+//Hello
+//Hello
+//Hello
+
+#include<stdio.h>
+
+// Demonstration of SEQUANCE
+
+int main()
+{
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    
+    return 0;
+}
