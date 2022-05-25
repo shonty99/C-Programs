6c44df65abd295b712f40039de890a4d6980494c
shonty99 <105678948+shonty99@users.noreply.github.com>
2022-05-25 14:16:09 +0530

    Create CountFactor.c

---
+++ CountFactor.c
@@ -0,0 +1,35 @@
+//Accept the number from user and return number of factors in that
+//Input : 6
+//Output : 3
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int CountFactor(int iNo)
+{
+    int iCnt = 0;
+    int iFactCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo/2; iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iFactCnt++;
+        }
+    }
+    return iFactCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    iRet = CountFactor(iValue);
+
+    printf("Number of factors are : %d\n",iRet);
+    return 0;
+}
